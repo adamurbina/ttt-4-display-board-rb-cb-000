dae6b4b0566d328af77a3c068b1b1b456a77f38b
Adam Urbina <adamurbina@gmail.com>
2018-05-01 23:28:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,6 @@ def display_board(board)
     puts(" #{board[6]} | #{board[7]} | #{board[8]}")
 end
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = ["X", "X", "X", "X", "X", "O", "O", "O", "O"]
 
 display_board(board)
